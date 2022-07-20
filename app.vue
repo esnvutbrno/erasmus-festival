42f8274e04e3912ea9c3e8c94421332b01d39f97
Josef Kolář <thejoeejoee@gmail.com>
2022-07-20 23:25:18 +0200

    Added tailwind

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
-    <h1>Erasmus Festival Brno 2022</h1>
+  <div class="max-w-2xl mx-auto">
+    <h1
+      class="text-3xl text-violet-600 font-bold py-8"
+    >
+      Erasmus Festival Brno 2022
+    </h1>
   </div>
 </template>
