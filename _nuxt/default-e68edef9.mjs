c436d0efd0c9983ea70ef1200f4005783653a461
thejoeejoee <thejoeejoee@users.noreply.github.com>
2022-07-22 10:42:46 +0000

    deploy: 970eb6d9feb23cb94fa1e33cc76e14463a289958

---
+++ _nuxt/default-e68edef9.mjs
@@ -0,0 +1,1 @@
+import{a as l,o as r,b as c,r as _,e,w as t,f as a,h as d}from"./entry-2c9a60e0.mjs";const f={},u=d("Erasmus Festival");function i(o,m){const s=a("Title"),n=a("Head");return r(),c("div",null,[_(o.$slots,"default"),e(n,null,{default:t(()=>[e(s,null,{default:t(()=>[u]),_:1})]),_:1})])}var h=l(f,[["render",i]]);export{h as default};
