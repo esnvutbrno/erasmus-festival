112b5747ea8f2692736e7d0c75cb757de3c147f1
thejoeejoee <thejoeejoee@users.noreply.github.com>
2022-09-04 06:55:39 +0000

    deploy: 0680acffe9ada451f2b09381cbffcec1c4c0dd16

--- _nuxt/useView-3de9a15e.mjs
+++
@@ -1,1 +0,0 @@
-import{X as s,Y as e,Z as o,$ as r}from"./entry-c57c220d.mjs";class t{constructor(a){this.router=a}async goTo(a){await e({hash:"#"+a.name}),a.goTo()}screenByName(a){return o.find(r,{name:a})}}const i=()=>new t(s());export{i as u};
