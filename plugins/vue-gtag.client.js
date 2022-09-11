6397ac5ba40d6ed8e569875f588f7c4090ad6983
Josef Kolář <thejoeejoee@gmail.com>
2022-09-11 15:52:27 +0200

    GA idk

--- plugins/vue-gtag.client.js
+++ plugins/vue-gtag.client.js
@@ -1,10 +1,12 @@
-import VueGtag from 'vue-gtag-next'
+import VueGtag, {trackRouter} from 'vue-gtag-next'
 import {defineNuxtPlugin, useRouter} from "nuxt/app";
+
 
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.use(VueGtag, {
         property: {
             id: 'UA-180183263-3'
         }
-    }, useRouter())
+    })
+    trackRouter(useRouter(), {useScreenview: true, skipSamePath: false})
 })
